7bbe2785b6ed595df93199782f6cef2cb23739d9
Griffith-Stites <gjstites@gmail.com>
2019-10-27 11:03:16 -0400

    Update legs to use information gathered in walk.ino

--- walkLib/walkLib.ino
+++ walkLib/walkLib.ino
@@ -2,13 +2,12 @@
 #include <Leg.h> // include the leg library
 
 // initialize all the legs
-// Leg(int liftPin, int rotatePin, int liftDirection,
-// int rotateDirection, int liftZero, int rotateZero)
-Leg leg1(2, 3, 1, 1, 90, 90);
-Leg leg2(4, 5, 1, 1, 90, 90);
-Leg leg3(6, 7, 1, 1, 90, 90);
-Leg leg4(8, 9, 1, 1, 90, 90);
-
+// Leg(int liftPin, int liftZero, int liftDirection,
+// int rotatePin, int rotateZero, int rotateDirection)
+Leg leg1(2, 105, -1, 3, 110, 1)
+Leg leg2(4, 80, 1, 5, 90, 1)
+Leg leg3(6, 90, 1, 7, 90, -1)
+Leg leg4(7, 95, -1, 8, 90, 1)
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -28,8 +27,8 @@ void loop() {
   leg1.backward(-30);
   leg4.backward(-30);
   // 2 and 3 forward
-  leg2.forward(30);
-  leg3.forward(30);
+  leg2.forward(0);
+  leg3.forward(0);
   delay(1000);
   // 2 and 3 down
   leg2.down(0);
@@ -42,7 +41,7 @@ void loop() {
   leg2.backward(-30);
   leg3.backward(-30);
   // 1 and 4 forward
-  leg1.forward(30);
-  leg4.forward(30);
+  leg1.forward(0);
+  leg4.forward(0);
   delay(1000);
 }
