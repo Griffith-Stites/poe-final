42c6c324399e20ea14797fb6e53eb20548992374
emmamack <42980990+emmamack@users.noreply.github.com>
2019-11-10 16:47:32 -0500

    On wall walking

--- v2/walk/walk.ino
+++ v2/walk/walk.ino
@@ -1,7 +1,5 @@
 #include <Servo.h> // Include the servo library
 
-// Code to just rotate the wheels
-
 // create servo objects to control rotation and magnet for each leg
 // front left
 Servo rotate1;
@@ -16,11 +14,11 @@ Servo mag3;
 Servo rotate4;
 Servo mag4;
 
-int bAngle = 45 // backward angle
-int fAngle = 0 // forward angle
+int bAngle = 110; // backward angle
+int fAngle = 70; // forward angle
 
-int magOn = 0 // mag on angle
-int magOff = 90 // mag off angle
+int magOn = 180; // mag on angle
+int magOff = 0; // mag off angle
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -34,38 +32,54 @@ void setup() {
   mag2.attach(11);
   mag3.attach(12);
   mag4.attach(13);
+
+  mag1.write(magOn);
+  mag2.write(magOn);
+  mag3.write(magOn);
+  mag4.write(magOn);
+
+  rotate1.write(fAngle);
+  rotate2.write(bAngle);
+  rotate3.write(fAngle);
+  rotate4.write(bAngle);
 }
 
 void loop() {
   Serial.println("start");
-  // release 1 and 2 magnets
   mag1.write(magOff);
-  mag2.write(magOff);
-  delay(500);
-  // slide 1 and 2 forward
-  rotate1.write(fAngle);
-  rotate2.write(fAngle);
-  delay(500);
-  // attach 1 and 2 magnets
-  mag1.write(magOn);
-  mag2.write(magOn);
-  delay(500);
-  // release 3 and 4 magnets
-  mag3.write(magOff);
-  mag4.write(magOff);
-  delay(500);
-  // slide 3 and 4 forward
-  rotate3.write(fAngle);
-  rotate4.write(fAngle);
-  delay(500);
-  // attach 3 and 4 magnets
-  mag3.write(magOn);
-  mag4.write(magOn);
-  delay(500);
-  // Slide all back
+  delay(1000);
   rotate1.write(bAngle);
-  rotate2.write(bAngle);
+  delay(1000);
+  mag1.write(magOn);
+  delay(1000);
+
+  Serial.println("start");
+  mag2.write(magOff);
+  delay(1000);
+  rotate2.write(fAngle);
+  delay(1000);
+  mag2.write(magOn);
+  delay(1000);
+  
+  mag3.write(magOff);
+  delay(1000);
   rotate3.write(bAngle);
+  delay(1000);
+  mag3.write(magOn);
+  delay(1000);
+
+  Serial.println("start");
+  mag4.write(magOff);
+  delay(1000);
+  rotate4.write(fAngle);
+  delay(1000);
+  mag4.write(magOn);
+  delay(1000);
+ 
+  // Slide all back
+  rotate1.write(fAngle);
+  rotate2.write(bAngle);
+  rotate3.write(fAngle);
   rotate4.write(bAngle);
-  delay(500);
+  delay(5000);
 }
